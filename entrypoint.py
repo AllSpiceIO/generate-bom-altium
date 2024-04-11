05e59472b003926a0a1e02791af16c5b4ce0aa17
Shrikanth Upadhayaya <shrik450@gmail.com>
2024-04-11 14:39:35 -0400

    Use github context for temp auth token

--- entrypoint.py
+++ entrypoint.py
@@ -7,7 +7,6 @@ import argparse
 import csv
 import json
 import os
-import subprocess
 import sys
 from contextlib import ExitStack
 
@@ -48,9 +47,6 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-
-    for entry in os.walk("/"):
-        print(entry)
 
     attributes_mapper = AttributesMapping.from_dict(json.loads(args.attributes_mapping))
 
